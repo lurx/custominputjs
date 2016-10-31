38971f7ed3f7e9d4a7a5ee7000965886f45f847d
DavidLevy2015 <david.l@investing.com>
2016-10-31 16:27:01 +0200

    minor js fixes

--- js/custominput.js
+++ js/custominput.js
@@ -1,133 +1,184 @@
 (function ($) {
-    $.fn.initNumberInput = function () {
-        $('.numInput I').unbind().click(function () {
-            var $direction = $(this).attr('data-dir'),      // = "plus" or "minus"
-                $minVal = $(this).parent().attr('data-min'),
-                $maxVal = $(this).parent().attr('data-max'),    
-                $step = 1,  //this is set later, 1 is just a default value
-                $stepAttr = $(this).parent().attr('data-step'),
-                $currentValue = Number($(this).parent().find('.numInputValue').text());
-            //Check and set steps.
-            if (typeof $stepAttr !== typeof undefined && $stepAttr !== false) {
-                $step = $stepAttr;
+
+    var defaultTypesSettings = {
+        number: {
+            values: {
+                defaultValue: 0,
+                minValue: null,
+                maxValue: null,
+                step: 1,
+                allowDecimal: false
+            },
+            plusBtn: {
+                btnClass: 'plusBtn',
+                btnText: '+'
+            },
+            minusBtn: {
+                btnClass: 'minusBtn',
+                btnText: '-'
             }
+        },
+        text: {
+            values: {
+                defaultValue: '',
+                placeholder: '',
+                maxLength: null
+            }
+        }
+    };
+
+    var defaultCSSClasses = {
+        number: 'numInput',
+        text: 'textInput'
+    };
+
+    var innerInputHtml = {
+        number: function(settings, instance) {
+            return '<i class="' + settings.number.plusBtn.btnClass + '" data-dir="plus">' + settings.number.plusBtn.btnText + '</i><i class="' + settings.number.minusBtn.btnClass + '" data-dir="minus">' + settings.number.minusBtn.btnText + '</i><span class="numInputValue" contenteditable>' + settings.number.values.defaultValue + '</span>';
+        },
+        text: function(settings) {
+            return '<input type="text" class="textInputValue" placeholder="' + settings.text.values.placeholder + '" value="' + settings.text.values.defaultValue + '" maxlength="' + settings.text.values.maxLength + '"></span><i class="fa fa-pencil"></i>';
+        }
+    };
+
+    var customeInputBehavior = {
+        number: NumberInputBehavior
+    }
+
+    var defaultErrorHtml = '<p style="color: red; font-weight: bold;">Choose input type!</p>';
+
+    var fontAwesomeImported = false;
+
+    function NumberStepper(stepper, numberInput) {
+        this.$stepper = $(stepper).click(onClick);
+
+        var direction = this.$stepper.attr('data-dir');      // = "plus" or "minus"
+        var settings = numberInput._numberInputSettings;
+        
+        function onClick() {
             //if plus/up button is clicked
-            if ($direction === 'plus') {
-                $currentValue += Number($step);
-                if ($currentValue >= $maxVal) {
-                    $currentValue = $maxVal;
+            if (direction === 'plus') {
+                settings.currentValue += settings.step;
+                if (settings.maxVal !== null && settings.currentValue >= settings.maxVal) {
+                    settings.currentValue = settings.maxVal;
+                }
+            } else { 
+            //if minus/down button is clicked
+                settings.currentValue -= settings.step;
+                if (settings.minVal !== null && settings.currentValue <= settings.minVal) {
+                    settings.currentValue = settings.minVal;
                 }
             }
-            else { //if minus/down button is clicked
-                $currentValue -= Number($step);
-                if ($currentValue <= $minVal) {
-                    $currentValue = $minVal;
-                }
-            }
-            //update value
-            $(this).parent().find('.numInputValue').text($currentValue);
+
+            settings.$inputvalueEl.text(settings.currentValue);
+        }
+    }
+
+
+    function NumberInputBehavior (el, numberSettings) {
+        var $inputValueEl = el.find('.numInputValue');
+
+        el._numberInputSettings = {
+            $inputvalueEl: $inputValueEl,
+            minVal: numberSettings.values.minValue,
+            maxVal: numberSettings.values.maxValue,
+            useDecimal: !!numberSettings.values.allowDecimal,
+            step: Number(numberSettings.values.step) || 1,
+            currentValue: Number($inputValueEl.text()),
+        };
+
+        el
+        .find('I')
+        .each(function() {
+            new NumberStepper(this, el);
         });
+
+        function onPressWithDecimal() {
+            $inputValueEl.val($inputValueEl.val().replace(/[^\d].+/, ""));
+            if ((event.which < 48 || event.which > 57)) {
+                event.preventDefault();
+            }
+        }
+
+        function onPressWithoutDecimal() {
+            var newValue = $inputValueEl.val().replace(/[^0-9\.]/g,'');
+            $inputValueEl.val(newValue);
+            if ((event.which != 46 || newValue.indexOf('.') != -1) && (event.which < 48 || event.which > 57)) {
+                event.preventDefault();
+            }
+        }
         
         //Allow numbers only in number input
-        $('[contenteditable]').on("keypress keyup blur",function (event) {
-            var $useDecimal = $(this).parent().attr('data-decimal');
-            //if allowDecimal is on
-            if ($useDecimal === true) {
-                $(this).val($(this).val().replace(/[^\d].+/, ""));
-                if ((event.which < 48 || event.which > 57)) {
-                    event.preventDefault();
-                }
-            } else { //if allowDecimal is off
-                $(this).val($(this).val().replace(/[^0-9\.]/g,''));
-                if ((event.which != 46 || $(this).val().indexOf('.') != -1) && (event.which < 48 || event.which > 57)) {
-                    event.preventDefault();
-                }
-            }                
-        });
+        $inputValueEl.on("keypress keyup blur",el._numberInputSettings.useDecimal ? onPressWithDecimal : onPressWithoutDecimal)
         
     }
-    $.fn.createInput = function (options) {
-        var defaultInputSettings = $.extend(true, {
+
+    function getInputType(options) {
+        return options || 'number';
+    }
+
+    function getInputTypeCustomeDefaultSettings(type) {
+        var obj = {};
+        obj[type] = defaultTypesSettings[type] || {};
+      return obj;
+    }
+
+    function getInputTypeCSSClass(type) {
+        return ' ' + defaultCSSClasses[type];
+    }
+
+
+    function importFontAwesome() {
+        if(fontAwesomeImported) return;
+
+        var fontCSSurl = 'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css';
+        $('HEAD').append('<link rel="stylesheet" href="' + fontCSSurl + '" type="text/css" />');
+        fontAwesomeImported = true;
+    }
+
+    function getInputSettings(options) {
+        var type = getInputType(options.inputType);
+        return $.extend(true, {
                 // These are the default settings, can be changed on use.
-                inputType: 'number',
+                inputType: type,
                 inputClass: '',
                 importFontAwesome: true,
-                theme: '',
-                number: {
-                    values: {
-                        defaultValue: 0,
-                        minValue: null,
-                        maxValue: null,
-                        step: 1,
-                        allowDecimal: false
-                    },
-                    plusBtn: {
-                        btnClass: 'plusBtn',
-                        btnText: '+'
-                    },
-                    minusBtn: {
-                        btnClass: 'minusBtn',
-                        btnText: '-'
-                    }
-                },
-                text: {
-                    values: {
-                        defaultValue: '',
-                        placeholder: '',
-                        maxLength: null
-                    }
-                }
-            }, options),
-            
-            $isTypeNumber = defaultInputSettings.inputType === 'number',
-            $isTypeText = defaultInputSettings.inputType === 'text',
-            $additionalClasses = '',
-            inputInnerHTML = '',
-            fontCSSurl = 'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css';
+                theme: 'theme-default ',
+            }, getInputTypeCustomeDefaultSettings(type), options);
+    }
+
+    function customInput (options) {
+        var settings = getInputSettings(options);
     
-        if (defaultInputSettings.importFontAwesome) {
-            $('HEAD').append('<link rel="stylesheet" href="' + fontCSSurl + '" type="text/css" />');
+        if (settings.importFontAwesome) {
+            importFontAwesome();
         }
-        if ($isTypeText) {
-            //Text input
-            if (defaultInputSettings.inputClass === '') {
-                defaultInputSettings.inputClass = 'textInput';
+
+        settings.inputClass += getInputTypeCSSClass(settings.inputType);
+
+        if(innerInputHtml[settings.inputType]) {
+            this
+            .empty()
+            .append(innerInputHtml[settings.inputType](settings, this))
+            .addClass([].concat(settings.inputClass, settings.theme).join(' '));
+
+            if(customeInputBehavior[settings.inputType]) {
+                // send the instance, and the custom settings for this perticular type
+                settings._cumstomBehavior = new NumberInputBehavior(this, settings[settings.inputType]);
             }
-            else {
-                defaultInputSettings.inputClass += 'textInput';
-            }
-            inputInnerHTML = '<input type="text" class="textInputValue" placeholder="' + defaultInputSettings.text.values.placeholder + '" value="' + defaultInputSettings.text.values.defaultValue + '" maxlength="' + defaultInputSettings.text.values.maxLength + '"></span><i class="fa fa-pencil"></i>';
+
+        } else {
+            this.html(defaultErrorHtml);
+                  
         }
-        else if ($isTypeNumber) {
-            //Number input
-            if (defaultInputSettings.inputClass === '') {
-                defaultInputSettings.inputClass = 'numInput';
-            }
-            else {
-                defaultInputSettings.inputClass += 'numInput';
-            }
-            inputInnerHTML = '<i class="' + defaultInputSettings.number.plusBtn.btnClass + '" data-dir="plus">' + defaultInputSettings.number.plusBtn.btnText + '</i><i class="' + defaultInputSettings.number.minusBtn.btnClass + '" data-dir="minus">' + defaultInputSettings.number.minusBtn.btnText + '</i><span class="numInputValue" contenteditable>' + defaultInputSettings.number.values.defaultValue + '</span>';
-        }
-        else {
-            // Error
-            inputInnerHTML = '<p style="color: red; font-weight: bold;">Choose input type!</p>';
-            this.html(inputInnerHTML);
-            return false;
-        }
-        if (defaultInputSettings.theme !== '') {
-            $additionalClasses += defaultInputSettings.theme;
-        }
-        else {
-            $additionalClasses += 'theme-default ';
-        }
-        return this.empty().append(inputInnerHTML).addClass([
-            defaultInputSettings.inputClass,
-            $additionalClasses
-        ].join(' ')).attr({
-            'data-min': defaultInputSettings.number.values.minValue,
-            'data-max': defaultInputSettings.number.values.maxValue,
-            'data-step': defaultInputSettings.number.values.step,
-            'data-decimal': defaultInputSettings.number.values.allowDecimal
-        }).initNumberInput();
+
+        return this;
+    }
+
+    $.fn.createInput = function (options) {
+        this.each(function() {
+            customInput.call($(this), options);
+        });
+        return this;
     };
 }(jQuery));
